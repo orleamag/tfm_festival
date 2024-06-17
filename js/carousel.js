27cb5f6da03985b0b548f09bdfca56d08daa39d6
Yamila Mevil <yamimevil@gmail.com>
2024-06-17 19:19:47 +0200

    carrousel-finally

--- js/carousel.js
+++ js/carousel.js
@@ -1,44 +1,25 @@
-let carousel = document.querySelector(".carousel");
-let carouselInner = document.querySelector(".carousel__inner");
-
-let pressed = false;
-let startX;
-let x;
-
-carousel.addEventListener("mousedown", (e) => {
-    pressed = true;
-    startX = e.offsetX - carouselInner.offsetLeft;
-    carousel.style.cursor = "grabbing";
-});
-
-carousel.addEventListener("mouseenter", () => {
-    carousel.style.cursor = "grab";
-});
-
-carousel.addEventListener("mouseup", () => {
-    carousel.style.cursor = "grab";
-    pressed = false;
-});
-
-carousel.addEventListener("mousemove", (e) => {
-    if (!pressed) return;
-    e.preventDefault();
-
-    x = e.offsetX;
-
-    carouselInner.style.left = `${x - startX}px`;
-    checkBoundary();
-});
-
-const checkBoundary = () => {
-    let outer = carousel.getBoundingClientRect();
-    let inner = carouselInner.getBoundingClientRect();
-
-    if (parseInt(carouselInner.style.left) > 0) {
-        carouselInner.style.left = "0px";
-    }
-
-    if (inner.right < outer.right) {
-        carouselInner.style.left = `-${inner.width - outer.width}px`;
-    }
+$.js = function (el) {
+    return $('[data-js=' + el + ']')
 };
+
+function carousel() {
+  $.js('carousel').slick({
+    infinite: false,
+    arrows: false,
+    dots: false,
+    autoplay: false,
+    speed: 1100,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      }]
+  });
+}
+
+carousel();
