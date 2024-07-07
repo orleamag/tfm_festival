e1bd267f9512c9d9fe50e3618bfdf1b7ac61d1b6
Orestes Alvarez Gonzalez <orleamag@gmail.com>
2024-07-07 18:46:45 +0200

    Tabbar-beging

--- js/navbar.js
+++ js/navbar.js
@@ -4,7 +4,10 @@ $(".tabbar ul li a").click(function (e) {
     setActive(li.parent(), li);
     return false;
 });
-function setActive(nav, li) {
+
+function setActive(nav, li, div) {
+    $("#fifteen_day").addClass("show");
+    $("#fifteen_day").removeClass("hide");
     nav.find("li").removeClass("active");
     li.addClass("active");
     nav.css("--x", li.position().left + li.outerWidth() / 2 + "px");
