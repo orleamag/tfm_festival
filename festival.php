6490273a93049b4bd4e8bc4a081ad91a9a7e2ac2
Orestes Alvarez Gonzalez <orleamag@gmail.com>
2024-07-25 17:40:38 +0200

    Page-festival-description

---
+++ festival.php
@@ -0,0 +1,25 @@
+<!-- Header -->
+<?php include 'header.html'; ?>
+    <main class="container">
+        <section class="fest">
+            <h1>Demoned</h1>
+            <h2>Festival de cine. Historia del Terror.</h2>
+            <div class="fest__content">
+                <div class="fest__cartel">
+                    <figure class="fest__img">
+                        <img src="img/cartel_festdemoned.webp" alt="">
+                    </figure>
+                </div>
+                <div class="fest__description">
+                    <p>Edgar Allan Poe, el maestro literario del terror fue su creador, pero la primera cinta considerada de terror fue, <strong>La mansión del diablo</strong>, dirigida e ideada por Georges Méliès, que se estrenó el 24 de diciembre de 1896, en París. Esto dió paso al cine del terror.</p>
+                    <p>En nuestro festival haremos un recorrido por esta maravillosa historia, brindándole al espectador una amplia varieddad de contenidos. Historia, años relevantes, clásicos y mucho más. en nuestro espacio contaremos con títulos tan reconocidos como: <strong>  El Exorcista, El Resplandor o Frankenstein.</strong> Daremos una mirada a este mundo fascinante que nos ha acompañado por tanto tiempo.</p>
+                    <p>Queremos inspirar el miedo en los espectadores, mostrarte el verdadero arte de no querer dejar de mirar la pantalla.</p>
+                    <p>Dicho esto, este Festival se centra en la historia viva del terror dentro del cine, mostrando varias títulos y clásicos de todos los tiempos, aquí verás películas galardonadas y muy queridas por los espectadores.</p>
+                    <p> Daremos un paseo terrorífico por la historia y los títulos más espeluznantes del cine de terror, una mirada a lo más oscuro de la mente de sus creadores, no olvides disfrutar del viaje y recuerda;</p>
+                    <cite>“Hagas lo que hagas, no te quedes dormido”</cite>
+                </div>
+            </div>
+        </section>
+    </main>
+    <!-- Footer -->
+    <?php include 'footer.html'; ?>
